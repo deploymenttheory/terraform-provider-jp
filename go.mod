5928f5d0a00915950dc5aa2297d3601a4152220f
ShocOne <shocone@deploymenttheory.com>
2023-10-26 07:47:26 +0100

    init

--- go.mod
+++ go.mod
@@ -1,29 +1,37 @@
 module github.com/hashicorp/terraform-provider-scaffolding-framework
 
-go 1.19
+go 1.21
+
+toolchain go1.21.3
 
 require (
+	github.com/deploymenttheory/go-api-sdk-jamfpro v0.0.31
+	github.com/deploymenttheory/terraform-provider-jamfpro v0.0.3
 	github.com/hashicorp/terraform-plugin-docs v0.16.0
 	github.com/hashicorp/terraform-plugin-framework v1.4.2
 	github.com/hashicorp/terraform-plugin-go v0.19.0
 	github.com/hashicorp/terraform-plugin-log v0.9.0
-	github.com/hashicorp/terraform-plugin-testing v1.5.1
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.29.0
+	github.com/stretchr/testify v1.8.4
 )
 
 require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
-	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.2.1 // indirect
 	github.com/Masterminds/sprig/v3 v3.2.2 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20230717121422-5aa5874ade95 // indirect
-	github.com/agext/levenshtein v1.2.2 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/agext/levenshtein v1.2.3 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/bgentry/speakeasy v0.1.0 // indirect
 	github.com/cloudflare/circl v1.3.3 // indirect
-	github.com/fatih/color v1.13.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.15.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-checkpoint v0.5.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
@@ -38,14 +46,13 @@ require (
 	github.com/hashicorp/logutils v1.0.0 // indirect
 	github.com/hashicorp/terraform-exec v0.19.0 // indirect
 	github.com/hashicorp/terraform-json v0.17.1 // indirect
-	github.com/hashicorp/terraform-plugin-sdk/v2 v2.29.0 // indirect
 	github.com/hashicorp/terraform-registry-address v0.2.2 // indirect
 	github.com/hashicorp/terraform-svchost v0.1.1 // indirect
 	github.com/hashicorp/yamux v0.0.0-20181012175058-2f1d1f20f75d // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/imdario/mergo v0.3.15 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mitchellh/cli v1.1.5 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
@@ -53,10 +60,12 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/oklog/run v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/posener/complete v1.2.3 // indirect
 	github.com/russross/blackfriday v1.6.0 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
@@ -68,7 +77,8 @@ require (
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	google.golang.org/grpc v1.57.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,85 +1,229 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
+// provider.go
 package provider
 
 import (
 	"context"
-	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+
+	"github.com/hashicorp/terraform-provider-scaffolding-framework/internal/client"
 )
 
-// Ensure ScaffoldingProvider satisfies various provider interfaces.
-var _ provider.Provider = &ScaffoldingProvider{}
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ provider.Provider = &jamfproProvider{}
+)
 
-// ScaffoldingProvider defines the provider implementation.
-type ScaffoldingProvider struct {
+// New is a helper function to simplify provider server and testing implementation.
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &jamfproProvider{
+			version: version,
+		}
+	}
+}
+
+// jamfproProvider is the provider implementation.
+type jamfproProvider struct {
 	// version is set to the provider version on release, "dev" when the
 	// provider is built and ran locally, and "test" when running acceptance
 	// testing.
 	version string
 }
 
-// ScaffoldingProviderModel describes the provider data model.
-type ScaffoldingProviderModel struct {
-	Endpoint types.String `tfsdk:"endpoint"`
+// jamfproProviderModel maps provider schema data to a Go type.
+type jamfproProviderModel struct {
+	InstanceName types.String `tfsdk:"instance_name"`
+	ClientID     types.String `tfsdk:"client_id"`
+	ClientSecret types.String `tfsdk:"client_secret"`
+	Debug        types.String `tfsdk:"debug"`
 }
 
-func (p *ScaffoldingProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "scaffolding"
+// Metadata returns the provider type name.
+func (p *jamfproProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "jamfpro"
 	resp.Version = p.version
 }
 
-func (p *ScaffoldingProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+// Schema defines the provider-level schema for configuration data.
+func (p *jamfproProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
+		Description: "Interact with jamfpro.",
 		Attributes: map[string]schema.Attribute{
-			"endpoint": schema.StringAttribute{
-				MarkdownDescription: "Example provider attribute",
-				Optional:            true,
+			"instance_name": schema.StringAttribute{
+				Description: "The Jamf Pro instance name. For mycompany.jamfcloud.com, define mycompany in this field.",
+				Optional:    true,
+			},
+			"client_id": schema.StringAttribute{
+				Description: "The Jamf Pro Client ID for authentication.",
+				Optional:    true,
+			},
+			"client_secret": schema.StringAttribute{
+				Description: "The Jamf Pro Client secret for authentication.",
+				Optional:    true,
+				Sensitive:   true,
+			},
+			"debug_mode": schema.StringAttribute{
+				Description: "Enable or disable debug mode for verbose logging.",
+				Optional:    true,
 			},
 		},
 	}
 }
 
-func (p *ScaffoldingProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data ScaffoldingProviderModel
+func (p *jamfproProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	tflog.Info(ctx, "Configuring jamfpro client")
 
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	// Retrieve provider data from configuration
+	var config jamfproProviderModel
+	diags := req.Config.Get(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// If practitioner provided a configuration value for any of the
+	// attributes, it must be a known value.
+
+	if config.InstanceName.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("instance_name"),
+			"Unknown jamfpro instance name",
+			"The provider cannot create the jamfpro API client as there is an unknown configuration value for the jamfpro instance name. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the instance_name environment variable.",
+		)
+	}
+
+	if config.ClientID.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("client_id"),
+			"Unknown jamfpro Client ID",
+			"The provider cannot create the jamfpro API client as there is an unknown configuration value for the jamfpro Client ID. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the client_id environment variable.",
+		)
+	}
+
+	if config.ClientSecret.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("client_secret"),
+			"Unknown jamfpro client secret",
+			"The provider cannot create the jamfpro API client as there is an unknown configuration value for the jamfpro client secret. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the client_secret environment variable.",
+		)
+	}
+
+	if config.Debug.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("debug_mode"),
+			"Unknown jamfpro debug mode setting",
+			"The provider cannot create the jamfpro API client as there is an unknown configuration value for the jamfpro debug mode. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the debug_mode environment variable.",
+		)
+	}
 
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Configuration values are now available.
-	// if data.Endpoint.IsNull() { /* ... */ }
+	// Default values to environment variables, but override
+	// with Terraform configuration value if set.
 
-	// Example client configuration for data sources and resources
-	client := http.DefaultClient
-	resp.DataSourceData = client
-	resp.ResourceData = client
+	// Default values to environment variables, but override with Terraform configuration value if set.
+	instanceName := os.Getenv("instance_name")
+	clientID := os.Getenv("client_id")
+	clientSecret := os.Getenv("client_secret")
+	debugMode := os.Getenv("debug_mode")
+
+	if !config.InstanceName.IsNull() {
+		instanceName = config.InstanceName.ValueString()
+	}
+
+	if !config.ClientID.IsNull() {
+		clientID = config.ClientID.ValueString()
+	}
+
+	if !config.ClientSecret.IsNull() {
+		clientSecret = config.ClientSecret.ValueString()
+	}
+
+	if !config.Debug.IsNull() {
+		debugMode = config.Debug.ValueString()
+	}
+
+	// If any of the expected configurations are missing, return
+	// errors with provider-specific guidance.
+
+	// If any of the expected configurations are missing, return errors with provider-specific guidance.
+	if instanceName == "" || clientID == "" || clientSecret == "" {
+		resp.Diagnostics.AddError(
+			"Missing Configuration",
+			"The provider cannot create the jamfpro API client as there is a missing or empty value for the configuration. "+
+				"Ensure all required values (instance_name, client_id, client_secret) are set.",
+		)
+		return
+	}
+
+	// Convert debugMode string to bool
+	debug, err := strconv.ParseBool(debugMode)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Invalid Debug Mode Value",
+			"Debug mode value must be 'true' or 'false'.",
+		)
+		return
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx = tflog.SetField(ctx, "instance_name", instanceName)
+	ctx = tflog.SetField(ctx, "client_id", clientID)
+	ctx = tflog.SetField(ctx, "client_secret", clientSecret)
+	ctx = tflog.MaskFieldValuesWithFieldKeys(ctx, "client_secret")
+
+	tflog.Debug(ctx, "Creating jamfpro client")
+
+	// Use the client package to create a new Jamf Pro client
+	providerConfig := client.ProviderConfig{
+		InstanceName: instanceName,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		DebugMode:    debug,
+	}
+	jamfProClient, diags := providerConfig.Client()
+
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to Create jamfpro API Client",
+			"An unexpected error occurred when creating the jamfpro API client. "+
+				"If the error is not clear, please contact the provider developers.\n\n"+
+				"jamfpro Client Error: "+err.Error(),
+		)
+		return
+	}
+
+	// Make the jamfpro client available during DataSource and Resource type Configure methods.
+	resp.DataSourceData = jamfProClient
+	resp.ResourceData = jamfProClient
+
+	tflog.Info(ctx, "Configured jamfpro client", map[string]any{"success": true})
 }
 
-func (p *ScaffoldingProvider) Resources(ctx context.Context) []func() resource.Resource {
-	return []func() resource.Resource{
-		NewExampleResource,
-	}
+// DataSources defines the data sources implemented in the provider.
+func (p *jamfproProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+	return []func() datasource.DataSource{}
 }
 
-func (p *ScaffoldingProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{
-		NewExampleDataSource,
-	}
-}
-
-func New(version string) func() provider.Provider {
-	return func() provider.Provider {
-		return &ScaffoldingProvider{
-			version: version,
-		}
-	}
+// Resources defines the resources implemented in the provider.
+func (p *jamfproProvider) Resources(_ context.Context) []func() resource.Resource {
+	return []func() resource.Resource{}
 }
